caaa7af13fb50388e400d79a02bdb50592d624fd
Kristo Palo <kristo.palo@gmail.com>
2025-06-18 17:00:52 +0300

    Read One Pair combinations from a JSON file, lazily.

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
 }
 
 kotlin {
