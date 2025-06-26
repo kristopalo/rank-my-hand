5430c3363e0f8e979f7e577ceb75697d4827f2ff
Kristo Palo <kristo.palo@gmail.com>
2025-06-26 15:43:29 +0300

    Add a very simple UI for the front end app using HTML, CSS and Javascript.

--- src/main/kotlin/com/bigstackbully/rankmyhand/controller/EvaluatorController.kt
+++ src/main/kotlin/com/bigstackbully/rankmyhand/controller/EvaluatorController.kt
@@ -5,6 +5,7 @@ import com.bigstackbully.rankmyhand.model.response.EvaluationResultResponse
 import com.bigstackbully.rankmyhand.service.EvaluationRequestTransformer
 import com.bigstackbully.rankmyhand.service.EvaluationResultTransformer
 import com.bigstackbully.rankmyhand.service.EvaluatorService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,6 +19,7 @@ class EvaluatorController(
     private val evaluationResultTransformer: EvaluationResultTransformer
 ) {
 
+    @CrossOrigin(origins = [])
     @PostMapping("/evaluate-hand")
     fun evaluate(@RequestBody evalReq: EvaluateHandRequest): EvaluationResultResponse {
         val evalHandCmd = evaluationRequestTransformer.toCommand(evaluateHandReq = evalReq)
