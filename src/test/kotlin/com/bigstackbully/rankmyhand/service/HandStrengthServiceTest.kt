8b455b0081be51d3d4cbfe5420c7f7409ff6241f
Kristo Palo <kristo.palo@gmail.com>
2026-02-01 00:49:26 +0200

    Remove a TODO and restore tests in HandStrengthServiceTest.

--- src/test/kotlin/com/bigstackbully/rankmyhand/service/HandStrengthServiceTest.kt
+++ src/test/kotlin/com/bigstackbully/rankmyhand/service/HandStrengthServiceTest.kt
@@ -1,7 +1,12 @@
 package com.bigstackbully.rankmyhand.service
 
 import com.bigstackbully.rankmyhand.model.Hand
+import com.bigstackbully.rankmyhand.model.HandStrength
 import com.bigstackbully.rankmyhand.model.enums.Card
+import com.bigstackbully.rankmyhand.model.enums.Ranking.*
+import com.bigstackbully.rankmyhand.utils.SINGLE_SPACE
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
 
 class HandStrengthServiceTest {
 
@@ -9,77 +14,72 @@ class HandStrengthServiceTest {
     private val handCombinationService = HandCombinationService(rankingService)
     private val handStrengthService = HandStrengthService(handCombinationService = handCombinationService)
 
-    private fun createHandFromRankNotation(rankNotationString: String): Hand {
-        // Create a hand with all cards of the same suit (spades) from rank notation
-        val cards = rankNotationString.mapNotNull { rankChar ->
-            val standardNotation = "${rankChar.uppercase()}s"
-            Card.fromShortNotation(standardNotation)
-        }
+    private fun createHandFromStandardNotation(standardNotationString: String): Hand {
+        val cards = standardNotationString.split(SINGLE_SPACE).mapNotNull { Card.fromShortNotation(it) }
         return Hand.of(cards)
     }
 
-    // TODO Kristo @ 02.11.2025 -> Uncomment back in, once input test data has been converted to standard notation
-//    @Test
-//    fun `should calculate hand strength for the strongest hand combination of each ranking`() {
-//        // arrange
-//        val testCases = mapOf(
-//            ROYAL_FLUSH to Pair("AKQJT", HandStrength(1, 1.0, 1, 1.0)),
-//            STRAIGHT_FLUSH to Pair("KQJT9", HandStrength(2, 0.999866, 1, 1.0)),
-//            FOUR_OF_A_KIND to Pair("AAAAK", HandStrength(11, 0.998660, 1, 1.0)),
-//            FULL_HOUSE to Pair("AAAKK", HandStrength(167, 0.977754, 1, 1.0)),
-//            FLUSH to Pair("AKQJ9", HandStrength(323, 0.956848, 1, 1.0)),
-//            STRAIGHT to Pair("AKQJT", HandStrength(1600, 0.785714, 1, 1.0)),
-//            THREE_OF_A_KIND to Pair("AAAKQ", HandStrength(1610, 0.784374, 1, 1.0)),
-//            TWO_PAIR to Pair("AAKKQ", HandStrength(2468, 0.669392, 1, 1.0)),
-//            ONE_PAIR to Pair("AAKQJ", HandStrength(3326, 0.554409, 1, 1.0)),
-//            HIGH_CARD to Pair("AKQJ9", HandStrength(6186, 0.171134, 1, 1.0))
-//        )
-//
-//        testCases.forEach { (ranking, rankingHandStrengthPair) ->
-//            // arrange
-//            val (rankNt, expHandStrength) = rankingHandStrengthPair
-//            val hand = createHandFromRankNotation(rankNt)
-//
-//            // act
-//            val actHandStrength = handStrengthService.calculateHandStrength(
-//                ranking = ranking,
-//                hand = hand
-//            )
-//
-//            // assert
-//            actHandStrength.shouldBe(expHandStrength)
-//        }
-//    }
-//
-//    @Test
-//    fun `should calculate hand strength for the weakest hand combination of each ranking`() {
-//        // arrange
-//        val testCases = mapOf(
-//            ROYAL_FLUSH to Pair("AKQJT", HandStrength(1, 1.0, 1, 1.0)),
-//            STRAIGHT_FLUSH to Pair("5432A", HandStrength(10, 0.998794, 9, 0.111111)),
-//            FOUR_OF_A_KIND to Pair("22223", HandStrength(166, 0.977888, 156, 0.00641)),
-//            FULL_HOUSE to Pair("22233", HandStrength(322, 0.956982, 156, 0.00641)),
-//            FLUSH to Pair("75432", HandStrength(1599, 0.785848, 1277, 0.000783)),
-//            STRAIGHT to Pair("5432A", HandStrength(1609, 0.784508, 10, 0.1)),
-//            THREE_OF_A_KIND to Pair("22243", HandStrength(2467, 0.669526, 858, 0.001166)),
-//            TWO_PAIR to Pair("33224", HandStrength(3325, 0.554543, 858, 0.001166)),
-//            ONE_PAIR to Pair("22543", HandStrength(6185, 0.171268, 2860, 0.00035)),
-//            HIGH_CARD to Pair("75432", HandStrength(7462, 0.000134, 1277, 0.000783))
-//        )
-//
-//        testCases.forEach { (ranking, rankingHandStrengthPair) ->
-//            // arrange
-//            val (rankNt, expHandStrength) = rankingHandStrengthPair
-//            val hand = createHandFromRankNotation(rankNt)
-//
-//            // act
-//            val actHandStrength = handStrengthService.calculateHandStrength(
-//                ranking = ranking,
-//                hand = hand
-//            )
-//
-//            // assert
-//            actHandStrength.shouldBe(expHandStrength)
-//        }
-//    }
+    @Test
+    fun `should calculate hand strength for the strongest hand combination of each ranking`() {
+        // arrange
+        val testCases = mapOf(
+            ROYAL_FLUSH to Pair("As Ks Qs Js Ts", HandStrength(1, 1.0, 1, 1.0)),
+            STRAIGHT_FLUSH to Pair("Ks Qs Js Ts 9s", HandStrength(2, 0.999866, 1, 1.0)),
+            FOUR_OF_A_KIND to Pair("As Ah Ad Ac Ks", HandStrength(11, 0.998660, 1, 1.0)),
+            FULL_HOUSE to Pair("As Ah Ad Kc Kd", HandStrength(167, 0.977754, 1, 1.0)),
+            FLUSH to Pair("As Ks Qs Js 9s", HandStrength(323, 0.956848, 1, 1.0)),
+            STRAIGHT to Pair("As Ks Qs Js Ts", HandStrength(1600, 0.785714, 1, 1.0)),
+            THREE_OF_A_KIND to Pair("As Ah Ad Ks Qs", HandStrength(1610, 0.784374, 1, 1.0)),
+            TWO_PAIR to Pair("As Ah Kc Kd Qs", HandStrength(2468, 0.669392, 1, 1.0)),
+            ONE_PAIR to Pair("As Ah Ks Qs Js", HandStrength(3326, 0.554409, 1, 1.0)),
+            HIGH_CARD to Pair("As Ks Qs Js 9s", HandStrength(6186, 0.171134, 1, 1.0))
+        )
+
+        testCases.forEach { (ranking, rankingHandStrengthPair) ->
+            // arrange
+            val (stdNt, expHandStrength) = rankingHandStrengthPair
+            val hand = createHandFromStandardNotation(stdNt)
+
+            // act
+            val actHandStrength = handStrengthService.calculateHandStrength(
+                ranking = ranking,
+                hand = hand
+            )
+
+            // assert
+            actHandStrength shouldBe expHandStrength
+        }
+    }
+
+    @Test
+    fun `should calculate hand strength for the weakest hand combination of each ranking`() {
+        // arrange
+        val testCases = mapOf(
+            ROYAL_FLUSH to Pair("As Ks Qs Js Ts", HandStrength(1, 1.0, 1, 1.0)),
+            STRAIGHT_FLUSH to Pair("5s 4s 3s 2s As", HandStrength(10, 0.998794, 9, 0.111111)),
+            FOUR_OF_A_KIND to Pair("2s 2h 2d 2c 3s", HandStrength(166, 0.977888, 156, 0.00641)),
+            FULL_HOUSE to Pair("2s 2h 2d 3c 3s", HandStrength(322, 0.956982, 156, 0.00641)),
+            FLUSH to Pair("7s 5s 4s 3s 2s", HandStrength(1599, 0.785848, 1277, 0.000783)),
+            STRAIGHT to Pair("5s 4s 3s 2s As", HandStrength(1609, 0.784508, 10, 0.1)),
+            THREE_OF_A_KIND to Pair("2s 2h 2d 4s 3s", HandStrength(2467, 0.669526, 858, 0.001166)),
+            TWO_PAIR to Pair("3s 3h 2c 2d 4s", HandStrength(3325, 0.554543, 858, 0.001166)),
+            ONE_PAIR to Pair("2s 2h 5s 4s 3s", HandStrength(6185, 0.171268, 2860, 0.00035)),
+            HIGH_CARD to Pair("7s 5s 4s 3s 2s", HandStrength(7462, 0.000134, 1277, 0.000783))
+        )
+
+        testCases.forEach { (ranking, rankingHandStrengthPair) ->
+            // arrange
+            val (stdNt, expHandStrength) = rankingHandStrengthPair
+            val hand = createHandFromStandardNotation(stdNt)
+
+            // act
+            val actHandStrength = handStrengthService.calculateHandStrength(
+                ranking = ranking,
+                hand = hand
+            )
+
+            // assert
+            actHandStrength.shouldBe(expHandStrength)
+        }
+    }
 }
